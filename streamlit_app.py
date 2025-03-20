ae1561197c6687021db44970406760920c771707
broneco <106168068+broneco@users.noreply.github.com>
2025-03-20 13:40:41 +0100

    Using SQL Alchemy
    Using a different package (last one was using Streamlit connect function that wasn't easy to configure for an odbc connector).

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import SQLAlchemy as sa
 
 st.title("Data Logging Test App")
 
@@ -10,11 +11,23 @@ database = st.secrets["dolphin_edu"]["database"]
 username = st.secrets["dolphin_edu"]["username"]
 password = st.secrets["dolphin_edu"]["password"]
 
+# Create the connection URL
 conn_str = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.connection(conn_str, type='sql')
+# Create the SQLAlchemy engine
+engine = sa.create_engine(conn_str)
 
-# Query and display the data you inserted
-API_usage_info = conn.query('SELECT TOP 10 * FROM [log].[openai_api_call]')
-st.dataframe(API_usage_info)
+# SQL query
+query = "SELECT TOP 10 * FROM [log].[openai_api_call];"
+
+# Read SQL query into Pandas DataFrame using SQLAlchemy
+df = pd.read_sql(query, con=engine)
+
+# Zahodit Primary Key, který se definuje v databázi
+df.drop(columns=["call_open_api_id"], inplace=True)
+
+# Vytvoření prázdné kopie pro zápis
+df2 = df.iloc[0:0].copy()
+
+
+st.dataframe(df2)
